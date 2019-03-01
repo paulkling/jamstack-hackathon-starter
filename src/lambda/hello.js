92ca9f292ed01303f94b0eaf426e14fed84000ac
Paul Kling <paulkling@users.noreply.github.com>
2019-03-01 12:01:56 -0600

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -9,7 +9,7 @@ export function handler(event, context, callback) {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      msg: 'Hello, World! ' + Math.round(Math.random() * 10),
+      msg: 'Hello, World yes! ' + Math.round(Math.random() * 10),
       ...obj,
     }),
   })
